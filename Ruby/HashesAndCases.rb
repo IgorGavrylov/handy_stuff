053efc244e70f0a41947179982f056c4626a6b21
Igor Gavrylov <sagot7@gmail.com>
2018-01-21 15:02:24 +0100

    final version of hash script

--- Ruby/HashesAndCases.rb
+++ Ruby/HashesAndCases.rb
@@ -1,18 +1,60 @@
-movies = Hash.new("Unknown movie!")
-movies["Matrix"] = 5
+movies = Hash.new
 
-puts "Say something!"
+movies[:test1] = 5
+movies[:test2] = 4
+
+print "What would you like to do? "
 choice = gets.chomp
 
 case choice.downcase
   when "add"
-  	puts "Added!"
+    print "Please provide movie title: "
+    title = gets.chomp.to_sym
+    if movies[title] == nil
+      print "And now rating for it, please: "
+      rating = gets.chomp.to_i
+      movies[title] = rating
+      puts "#{title} with rating #{rating} was added to your DB."
+    else
+      puts "#{title} is already in your database!"
+    end
+    puts "Current state of your DB:"
+    movies.each {|movie, rating|
+      puts "#{movie}: #{rating}"}
+
   when "update"
-  	puts "Updated!"
+    print "Please provide movie title: "
+    title = gets.chomp.to_sym
+    if movies[title] != nil
+      print "And now rating for it, please: "
+      rating = gets.chomp.to_i
+      movies[title] = rating
+      puts "#{title} with rating #{rating} was updated in your DB."
+    else
+      puts "#{title} is missing from your database!"
+    end
+    puts "Current state of your DB:"
+    movies.each {|movie, rating|
+      puts "#{movie}: #{rating}"}
+
   when "display"
-  	puts "Movies!"
+    puts "Current state of your DB:"
+    movies.each {|movie, rating|
+      puts "#{movie}: #{rating}"}
+
   when "delete"
-  	puts "Deleted!"
+    print "Please provide movie title: "
+    title = gets.chomp.to_sym
+    if movies[title] != nil
+      movies.delete(title)
+      puts "#{title} deleted from your database!"
+    else
+      puts "#{title} is not present in your database!"
+    end
+    puts "Current state of your DB:"
+    movies.each {|movie, rating|
+      puts "#{movie}: #{rating}"}
+
   else
-  	puts "Error! Unknown command: #{choice.downcase}"
+    puts "Error! Unknown command: #{choice.downcase}"
 end
